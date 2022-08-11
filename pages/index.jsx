49a1bc9af54d62ec9e184adb722c08bcc934c5f1
tgh15 <muhteguhadhiputra15@gmail.com>
2022-08-11 11:38:03 +0800

    banner fixed

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,28 +15,28 @@ export default function Home() {
       <div className="w-full bg-blue-100 pt-4">
         <h1 className="font-bold text-2xl">Reservasi Sekarang</h1>
         <div className="w-80">
-          <label for="price" class="block text-sm font-medium text-gray-700">
+          <label htmlFor="price" className="block text-sm font-medium text-gray-700">
             Price
           </label>
-          <div class="mt-1 relative rounded-md shadow-sm">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <span class="text-gray-500 sm:text-sm"> $ </span>
+          <div className="mt-1 relative rounded-md shadow-sm">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <span className="text-gray-500 sm:text-sm"> $ </span>
             </div>
             <input
               type="text"
               name="price"
               id="price"
-              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
               placeholder="0.00"
             />
-            <div class="absolute inset-y-0 right-0 flex items-center">
-              <label for="currency" class="sr-only">
+            <div className="absolute inset-y-0 right-0 flex items-center">
+              <label htmlFor="currency" className="sr-only">
                 Currency
               </label>
               <select
                 id="currency"
                 name="currency"
-                class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+                className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
               >
                 <option>USD</option>
                 <option>CAD</option>
